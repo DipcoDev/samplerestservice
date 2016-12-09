79c8d739fe7bb30f40e3d0172fc6a303b8ecf9db
Charley <charley.hastings@newinsuranceco.com>
2016-12-09 09:22:04 -0600

    Updated metadata

--- src/main/java/hello/Greeting.java
+++ src/main/java/hello/Greeting.java
@@ -6,14 +6,20 @@ package hello;
 public class Greeting {
     private final long id;
     private final String content;
+    private final String metadata;
+
     public Greeting(long id, String content){
         this.id = id;
         this.content = content;
+        this.metadata = "this is some extra content";
     }
     public long getId(){
         return id;
     }
     public String getContent(){
         return content;
+    }
+    public String getMetadata() {
+        return metadata;
     }
 }
